30538e5d3267c8023a9385bc4c7783c42daa44f4
tanyang <122074809@qq.com>
2017-10-17 12:47:12 +0800

    fixed layoutManager highlight

--- TYTextKitDemo/TYTextKit/TextKit/TYLayoutManager.m
+++ TYTextKitDemo/TYTextKit/TextKit/TYLayoutManager.m
@@ -41,7 +41,7 @@
 }
 
 - (void)fillBackgroundRectArray:(const CGRect *)rectArray count:(NSUInteger)rectCount forCharacterRange:(NSRange)charRange color:(UIColor *)color {
-    if (_highlightBackgroudRadius > 0 && NSIntersectionRange(_highlightRange, charRange).length != charRange.length) {
+    if (_highlightRange.length == 0 || NSIntersectionRange(_highlightRange, charRange).length != charRange.length) {
         [super fillBackgroundRectArray:rectArray count:rectCount forCharacterRange:charRange color:color];
         return;
     }
